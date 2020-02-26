89bfa1f62cef5547a045fd3f40c6766ea5dd8e88
Peter Rigole <peter.rigole@atomicbits.io>
2020-02-26 20:30:12 +0100

    Layout improvements. Content added.

--- js/site-utils.js
+++ js/site-utils.js
@@ -1,14 +1,37 @@
-
 // When the user clicks anywhere outside of the navbar selection area, close it.
-$(document).mouseup(function(e)
-{
+$(document).mouseup(function (e) {
     var container = $("#navbarToggleExternalContent");
     var visibleArea = $("#navbarVisibleArea");
 
     // if the target of the click isn't the container nor a descendant of the container
-    if (!visibleArea.is(e.target) && visibleArea.has(e.target).length === 0)
-    {
+    if (!visibleArea.is(e.target) && visibleArea.has(e.target).length === 0) {
         container.removeClass('show');
-        e.preventDefault();
+        // e.preventDefault();
     }
 });
+
+
+$(
+    function () {
+        var bgvs = $('.bg-video');
+
+        bgvs.each(
+            function (i, bgv) {
+
+                if ($(bgv).hasClass("lazy-loading")) {   // if ($(bgv).is(':visible')) { ...
+
+                    $('source', bgv).each(
+                        function () {
+                            var el = $(this);
+                            el.attr('src', el.attr('data-src'));
+                        }
+                    );
+
+                    bgv.load();
+                    $(bgv).removeClass("lazy-loading")
+                }
+            }
+        );
+
+    }
+);
